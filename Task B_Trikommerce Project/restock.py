9c2c21a7b6836051e27cdf04b3c2ff73380c7165
TobyWestBU <s5708718@bournemouth.ac.uk>
2024-11-18 19:04:33 +0000

    The restock_inventory function will update inventory levels given each day. It will take the number of available items, past inventory records and current day sales. On the firstday it will restock the store to 2000 shirts. On the 7th day it will calculate the amount of stock needed to hit the maximum capacity of 2000.

--- Task B_Trikommerce Project/restock.py
+++ Task B_Trikommerce Project/restock.py
@@ -17,5 +17,14 @@ available_items:(integer) This function returns this integer which updates the a
 
 The function will also update the inventory_records (For restocking) for a  given current day. It will also return "available_items".
     '''
+    if current_day == 0:
+        restocked_units = 2000 
+        inventory_records.append([current_day, 0, restocked_units, available_items])
+        
+    elif current_day % 7 == 0:
+        restocked_units = (2000 - available_items)
+        available_items = 2000
+        inventory_records.append([current_day, 0, restocked_units, available_items])
 
+        
     return available_items
